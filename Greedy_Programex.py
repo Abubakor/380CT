5ff15815b037c82fd1c8c3d7ad6e9028c84daad5
Tpr2312 <tprstar@gmail.com>
2017-01-09 19:29:27 +0000

    Update Greedy_Programex.py

--- Greedy_Programex.py
+++ Greedy_Programex.py
@@ -3,11 +3,14 @@ import random
 numbers = []
 left = []
 right = []
+balance = 0
 
 def farthestVal(num):
 	largest_delta = 0 
+	largest_key = 0
 	for i in numbers:
 		if(abs(num-i) > largest_delta): 
+			largest_delta = abs(num-i)
 			large_key = i
 	return large_key
 
@@ -20,11 +23,23 @@ for j in range(12):
 		if(balance < 0): 
 			far_val = farthestVal(balance) 
 			right.append(far_val)
+			numbers.remove(far_val)
+			balance += far_val
 		else: 
 			far_val = farthestVal(balance)  
 			left.append(far_val) 
 			numbers.remove(far_val)
 			balance -= far_val 
+	else:
+		far_val = farthestVal (balance)
+		left.append(far_val)
+		numbers.remove(far_val)
+		balance -= far_val
+
+print ('left: ')
+print (left)
+print ('right: ') 
+print (right)
 
 print ('balance: ')
 print (balance)
